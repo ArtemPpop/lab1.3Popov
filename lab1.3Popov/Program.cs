9a8138d9a4ac28c58ab09f7a8e3836dccadc33d6
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-09-22 18:45:04 +0200

    Добавьте файлы проекта.

---
+++ lab1.3Popov/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите n:");
+int n = int.Parse(Console.ReadLine());
+int a = n / 100;
+int b = (n / 10) % 10;
+int c = n % 10;
+
+Console.WriteLine("Все перестановки:");
+Console.WriteLine($"{a}{b}{c}");
+Console.WriteLine($"{a}{c}{b}");
+Console.WriteLine($"{b}{a}{c}");
+Console.WriteLine($"{b}{c}{a}");
+Console.WriteLine($"{c}{a}{b}");
+Console.WriteLine($"{c}{b}{a}");
